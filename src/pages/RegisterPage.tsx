a6c80b0f0ba924d34b2ac2a31d62c6ba801c9abd
Lev <leevkrasnov@gmail.com>
2025-05-30 22:22:53 +0500

    fix (styles)

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -13,12 +13,12 @@ export default function RegisterPage() {
         <section>
           <Link
             to="/Auth"
-            className="flex items-center text-xl absolute left-4 top-20 lg:top-12 gap-1 text-gray-600 hover:text-flame duration-400 hover:scale-105"
+            className="flex items-center text-lg md:text-xl absolute left-4 top-20 lg:top-12 gap-1 text-gray-600 hover:text-flame duration-400 hover:scale-105"
           >
-            <ChevronLeft size={40} strokeWidth={1} />
+            <ChevronLeft size={35} strokeWidth={1} />
             Назад
           </Link>
-          <h1 className="text-3xl lg:text-5xl font-semibold lg:mt-32 mt-42 mb-8 lg:mb-18 px-8 lg:px-10">
+          <h1 className="text-3xl lg:text-5xl font-semibold lg:mt-32 mt-42 mb-8 lg:mb-18">
             РЕГИСТРАЦИЯ
           </h1>
           {/* <h2 className="mt-14 mb-3 text-lg px-10 text-gray-600">
@@ -26,7 +26,7 @@ export default function RegisterPage() {
           </h2> */}
         </section>
 
-        <section className="pb-20 px-8 lg:px-18">
+        <section className="pb-20 px-8 lg:px-10">
           <RegisterForm />
         </section>
       </div>
